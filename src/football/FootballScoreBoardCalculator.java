3d58314c37adbdee50e12ab52b26774d10adde98
aneen2510 <aneenantony3@gmail.com>
2022-01-22 18:04:04 +0100

    Rearranged folder structure and formatted files

--- src/football/FootballScoreBoardCalculator.java
+++ src/football/FootballScoreBoardCalculator.java
@@ -7,9 +7,9 @@ import java.util.List;
 public class FootballScoreBoardCalculator {
 
     private static ArrayList<FootballMatchDetails> historyOfMatches = new ArrayList<>();
-    private static HashMap<String,FootballMatchDetails>liveMatchDetails = new HashMap<>();
+    private static HashMap<String, FootballMatchDetails> liveMatchDetails = new HashMap<>();
 
-    public static String startGame(String homeTeam,String awayTeam) {
+    public static String startGame(String homeTeam, String awayTeam) {
 
         if (homeTeam == null || awayTeam == null) {
             return "Please enter Team name";
@@ -24,40 +24,39 @@ public class FootballScoreBoardCalculator {
         return output;
     }
 
-    public static String updateScore(String homeTeam,int homeTeamScore,String awayTeam,int awayTeamScore){
+    public static String updateScore(String homeTeam, int homeTeamScore, String awayTeam, int awayTeamScore) {
 
         FootballMatchDetails footballMatch = liveMatchDetails.get(homeTeam);
-        footballMatch.setHomeTeamScore(footballMatch.getHomeTeamScore()+homeTeamScore);
-        footballMatch.setAwayTeamScore(footballMatch.getAwayTeamScore()+awayTeamScore);
-        liveMatchDetails.replace(homeTeam,footballMatch);
-        String output = "Score : "+homeTeam + "="+footballMatch.getHomeTeamScore()+ ":" + awayTeam + "="+footballMatch.getAwayTeamScore();
+        footballMatch.setHomeTeamScore(footballMatch.getHomeTeamScore() + homeTeamScore);
+        footballMatch.setAwayTeamScore(footballMatch.getAwayTeamScore() + awayTeamScore);
+        liveMatchDetails.replace(homeTeam, footballMatch);
+        String output = "Score : " + homeTeam + "=" + footballMatch.getHomeTeamScore() + ":" + awayTeam + "=" + footballMatch.getAwayTeamScore();
         System.out.println(output);
         return output;
     }
 
-    public static String endGame(String homeTeam,String awayTeam){
+    public static String endGame(String homeTeam, String awayTeam) {
 
         FootballMatchDetails footballMatch = liveMatchDetails.get(homeTeam);
         historyOfMatches.add(footballMatch);
 
         liveMatchDetails.remove(homeTeam);
-        String output = homeTeam + " vs "+ awayTeam + " Match has ended with score "+footballMatch.getHomeTeamScore()+ ":"+ footballMatch.getAwayTeamScore() ;
+        String output = homeTeam + " vs " + awayTeam + " Match has ended with score " + footballMatch.getHomeTeamScore() + ":" + footballMatch.getAwayTeamScore();
         System.out.println(output);
         return output;
 
     }
 
-    public static List<FootballMatchDetails> historyOfMatches(){
+    public static List<FootballMatchDetails> historyOfMatches() {
 
 
         historyOfMatches.forEach(match ->
-                System.out.println(match.getHomeTeam()+" - "+match.getAwayTeam() +" : " +match.getHomeTeamScore()
-                        +" - "+match.getAwayTeamScore()));
+                System.out.println(match.getHomeTeam() + " - " + match.getAwayTeam() + " : " + match.getHomeTeamScore()
+                        + " - " + match.getAwayTeamScore()));
 
         return historyOfMatches;
 
     }
-
 
 
 }
